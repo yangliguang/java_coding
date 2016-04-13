f03d4ef6562ce0f46a90fb98b7381bf0545c3e1f
Young <admin@yanglg.me>
2016-04-13 18:39:35 +0800

    回文字符串将时间复杂度降到为O(1)

--- src/leetcode/before100/NO_005.java
+++ src/leetcode/before100/NO_005.java
@@ -35,11 +35,49 @@ public class NO_005 {
 		}
 		return true;
 	}
+	
+	
+	public String longestPalindrome2(String s) {
+		if(s.length() < 2)
+			return s;
+		int max = 1,p=0,q=0;
+		char[] c = s.toCharArray();
+		for(int i = 1; i < s.length(); i++){
+			int start = i;
+			int end = i;
+			while(start>=0&&end<c.length&&c[start]==c[end]){
+				if(end-start+1>max){
+					max++;
+					p=start;
+					q=end;
+				}
+				start--;
+				end++;
+			}
+		}
+		for(int i = 0; i < s.length()-1; i++){
+			int start = i;
+			int end = i+1;
+			while(start>=0&&end<c.length&&c[start]==c[end]){
+				if(end-start+1>max){
+					max++;
+					p=start;
+					q=end;
+				}
+				start--;
+				end++;
+			}
+			
+		}
+		return s.substring(p, q+1);
+	}
+	
 	public static void main(String[] args) {
 		NO_005 m = new NO_005();
 		long t1 = System.currentTimeMillis();
+		//String s = "abc";
 		String s = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
-		System.out.println(m.longestPalindrome(s));
+		System.out.println(m.longestPalindrome2(s));
 		long t2 = System.currentTimeMillis();
 		System.out.println(t2-t1);
 		//System.out.println(m.cal("abcdcba"));
