f4a39e11ca6be67845fe5cdc6b5e21a1a246f678
Young <admin@yanglg.me>
2016-05-16 15:20:00 +0800

    整数逆序

--- src/leetcode/NO_007.java
+++ src/leetcode/NO_007.java
@@ -11,46 +11,17 @@ public class NO_007 {
 		NO_007 m = new NO_007();
 		System.out.println("max:"+Integer.MAX_VALUE);
 		System.out.println("min:"+Integer.MIN_VALUE);
-		System.out.println(m.reverse(-2147483648));
-		System.out.println(m.reverse2(2147483646));
+		System.out.println(m.rever(1234567));
 	}
-	public int reverse(int x){
-		int r = 0;
-		while(x!=0){
-			long l = (long)r*10+x%10;
-			if(l > Integer.MAX_VALUE || l < Integer.MIN_VALUE)
-				return 0;
-			r = (int) l;
+	public int rever(int x){
+		long r = 0;
+		while(x != 0){
+			r = r*10 + x%10;
 			x /= 10;
 		}
-		return r;
-	}
-	public int reverse2(int x) {
-		if(x>=0){
-			long l = Long.parseLong(rev(x+""));
-			if(l > Integer.MAX_VALUE)
-				return 0;
-			return Integer.parseInt(rev(x+""));
-		} else{
-			if(x == Integer.MIN_VALUE){
-				return 0;
-			} else{
-				long l = Long.parseLong(rev(-x+""))*-1;
-				System.out.println();
-				if(l < Integer.MIN_VALUE)
-					return 0;
-				return Integer.parseInt(rev(-x+""))*-1;
-			}
-			
-		}
-    }
-	
-	public String rev(String s){
-		String res = new String();
-		char[] c = s.toCharArray();
-		for(int i = c.length-1; i >= 0; i--){
-			res += c[i];
-		}
-		return res;
+		if(r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE)
+			return (int)r;
+		else
+			return 0;
 	}
 }
