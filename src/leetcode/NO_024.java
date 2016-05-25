0697ca4c4e2fb2953ffb17c651d78c888faf1647
yangliguang <admin@yanglg.me>
2016-05-25 22:14:44 +0800

    链表交换

---
+++ src/leetcode/NO_024.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * Author:Young
+ * Class Comment:
+ * Date: 2016年5月23日下午4:15:12
+ */
+public class NO_024 {
+	public static void main(String[] args) {
+		ListNode n = new ListNode(1);
+		n.next = new ListNode(2);
+		n.next.next = new ListNode(3);
+		n.next.next.next = new ListNode(4);
+		n.next.next.next.next = new ListNode(5);
+		ListNode b = new NO_024().swapPairs(n);
+		System.out.println("finish"+b);
+	}
+	public ListNode swapPairs(ListNode head) {
+		if(head == null)
+			return null;
+		if(head.next == null)
+			return head;
+		
+		ListNode p1 = head, p2 = head.next, p3 = null;
+		p1.next = null;
+		p3 = p2.next;
+		p2.next = null;
+		p2.next = p1;
+		p1.next = p3;
+		
+		if(p1.next != null)
+			p1.next = swapPairs(p1.next);
+		
+		return p2;
+	}
+}
